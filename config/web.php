5d320f985a39cd8b21c4090b84b855e09013f371
Zeberti19(2) <31765032+Zeberti19(2)@users.noreply.github.com>
2019-03-06 18:24:03 +0500

    Установлен уникальный ИД приложения

--- config/web.php
+++ config/web.php
@@ -4,7 +4,7 @@ $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
 $config = [
-    'id' => 'basic',
+    'id' => 'zshop.yii5391',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
