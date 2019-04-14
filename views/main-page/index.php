319c82bb32e7fd76e56b152031565dd88bb68913
Zeberti19 <31765032+Zeberti19@users.noreply.github.com>
2019-04-14 18:17:31 +0500

    [image_path_empty_task] Теперь изображение по-умолчанию указывается в PHP коде, а не в БД

--- views/main-page/index.php
+++ views/main-page/index.php
@@ -6,7 +6,14 @@ use yii\helpers\Html;
 <?php foreach($GoodsCategory as $Cat)
 {
     $catNameEncoded=Html::encode($Cat->name);
-    $imageParams=getimagesize('images/'.$Cat->image_path);
+    $imagePath='images/'.$Cat->image_path;
+    $imageParams=@getimagesize($imagePath);
+    //если изображения нет, то ставим изображение по-умолчанию
+    if (!$imageParams)
+    {
+        $imagePath='images/empty.png';
+        $imageParams=getimagesize($imagePath);
+    }
     //если высота изображения больше ширины, то выравниваем изображение по высоте с учетом пропорций. В противном случаи - по ширине
     ($imageParams and $imageParams[1]>$imageParams[0]) ? $imageVerticalIs=true : $imageVerticalIs=false;
 ?>
@@ -14,7 +21,7 @@ use yii\helpers\Html;
         <div class="goods-category__image-container">
             <?php ?>
             <img class="goods-category__image<?=($imageVerticalIs ? ' goods-category__image_vertical' : '')?>"
-                 src="images/<?= Html::encode($Cat->image_path) ?>" alt="Изображение категории товаров &quot;<?= $catNameEncoded ?>&quot;"
+                 src="<?= Html::encode($imagePath) ?>" alt="Изображение категории товаров &quot;<?= $catNameEncoded ?>&quot;"
                  height="300" width="300"
             >
         </div>
