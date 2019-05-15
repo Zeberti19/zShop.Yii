ebbbdfa80fd48bb0d1d91f5180c7d3aff6967ca2
Zeberti19 <31765032+Zeberti19@users.noreply.github.com>
2019-05-15 14:13:59 +0500

    [image_return2_task] Добавлены новые изображения для товаров

--- config/params.php
+++ config/params.php
@@ -2,5 +2,6 @@
 
 return [
     'adminEmail' => 'admin@example.com',
-    'image_prefix' => 'https://goofy-raman-79b79e.netlify.com/'
+    'image_prefix' => 'https://kind-edison-2e35aa.netlify.com/',
+    //'image_prefix' => 'images/'
 ];
