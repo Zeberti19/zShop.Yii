363219240457a6eb336a9e7da92f8edb39437adc
Zeberti19 <31765032+Zeberti19@users.noreply.github.com>
2019-10-24 20:58:23 +0500

    [error_handler_task] Категории товаров. Добавлено логирование ошибки отсуствия изображения категории товаров

--- views/main-page/index.php
+++ views/main-page/index.php
@@ -1,6 +1,9 @@
 <?php
+use app\components\helpers\ErrorHandler as Error;
 use yii\helpers\Html;
+
 /**@var app\models\CategoryGoods $CategoriesGoods*/
+/**@var string $errPrefix*/
 ?>
 <div class="what-interested">Что интересует?</div>
 <?php foreach($CategoriesGoods as $Cat)
@@ -11,6 +14,7 @@ use yii\helpers\Html;
     //если изображения нет, то ставим изображение по-умолчанию
     if (!$imageParams)
     {
+        Error::handle(__FILE__ .':' .__LINE__,'CG_IMG_NF', "{$errPrefix}Не найден файл с изображением категории товаров. ИД категории: {$Cat->id}; Путь до изображения: {$imagePath}" );
         $imagePath=Yii::$app->params['image_prefix'].'empty.png';
         $imageParams=getimagesize($imagePath);
     }
